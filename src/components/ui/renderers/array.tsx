cfc19fd6a47709ed64d85143942082f455b36ffe
Abhinav5383 <itsabhinav5383@gmail.com>
2026-01-10 13:37:20 +0530

    add drag-and-drop functionality to ArrayRenderer

--- src/components/ui/renderers/array.tsx
+++ src/components/ui/renderers/array.tsx
@@ -1,9 +1,34 @@
+import type { DragendEventData } from "@formkit/drag-and-drop";
+import { useDragAndDrop } from "@formkit/drag-and-drop/solid";
 import { For, Show } from "solid-js";
 import type { ArrayNode, TranslationNode } from "~/lib/types";
 import { NodeRenderer } from "./node";
 import type { NodeRendererProps } from "./types";
 
 export function ArrayRenderer(props: NodeRendererProps<ArrayNode>) {
+    const initialValues = props.node.value.map((item, index) => ({ index, item }));
+
+    const [parentRef, nodeValues] = useDragAndDrop<HTMLDivElement, (typeof initialValues)[number]>(
+        initialValues,
+        {
+            dragHandle: ".drag-handle",
+            dropZoneClass: "drop-location",
+            selectedClass: "dragging",
+            draggable: () => props.isEditable,
+            onDragend: (_data) => {
+                const data = _data as unknown as DragendEventData<(typeof initialValues)[number]>;
+                const newArray = data.values.map((item) => {
+                    return { ...item.item };
+                });
+
+                props.onChange(props.path, {
+                    ...props.node,
+                    value: newArray,
+                });
+            },
+        },
+    );
+
     function handleChange(index: number, newNode: TranslationNode) {
         if (newNode.type !== "string" && newNode.type !== "string_template") return;
 
@@ -34,14 +59,12 @@ export function ArrayRenderer(props: NodeRendererProps<ArrayNode>) {
     return (
         <div class="node-array">
             <span class="token token-bracket">{"["}</span>
-            <div class={`array-items ${props.isEditable ? "editable" : ""}`}>
-                <For each={props.node.value}>
-                    {(item, index) => (
+            <div class={`array-items ${props.isEditable ? "editable" : ""}`} ref={parentRef}>
+                <For each={nodeValues()}>
+                    {({ item }, index) => (
                         <div class="array-item">
                             <Show when={props.isEditable}>
-                                <button type="button" class="drag-handle" tabindex={-1}>
-                                    ⠿
-                                </button>
+                                <span class="drag-handle">⠿</span>
                             </Show>
 
                             <NodeRenderer
@@ -59,6 +82,7 @@ export function ArrayRenderer(props: NodeRendererProps<ArrayNode>) {
                     )}
                 </For>
             </div>
+
             <div>
                 <span class="token token-bracket">{"]"}</span>
                 {props.postInlineContent}
