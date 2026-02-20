1050c3e6b26cd5fd854e5211338bdc61c055ab3a
Abhinav5383 <itsabhinav5383@gmail.com>
2026-02-20 10:44:50 +0530

    ee

--- src/lib/local-store.ts
+++ src/lib/local-store.ts
@@ -1,24 +1,24 @@
-import type { ObjectNode } from "./types";
+// import type { ObjectNode } from "./types";
 
-function saveTranslationWork(obj: ObjectNode, locale: string, fileName: string) {}
+// function saveTranslationWork(obj: ObjectNode, locale: string, fileName: string) {}
 
-function getDbInstance() {
-    const db = window.indexedDB.open("saves", 1);
+// function getDbInstance() {
+//     const db = window.indexedDB.open("saves", 1);
 
-    db.onerror = (ev) => {
-        console.error("IndexedDB error:", ev);
-    };
+//     db.onerror = (ev) => {
+//         console.error("IndexedDB error:", ev);
+//     };
 
-    db.onupgradeneeded = (ev) => {
-        const evTarget = ev.target;
-        if (!(evTarget instanceof IDBOpenDBRequest)) return;
+//     db.onupgradeneeded = (ev) => {
+//         const evTarget = ev.target;
+//         if (!(evTarget instanceof IDBOpenDBRequest)) return;
 
-        const db = evTarget.result;
+//         const db = evTarget.result;
 
-        const objStore = db.createObjectStore("translations", {});
-    };
-}
+//         const objStore = db.createObjectStore("translations", {});
+//     };
+// }
 
-function key(locale: string, fileName: string) {
-    return `${locale}/${fileName}`;
-}
+// function key(locale: string, fileName: string) {
+//     return `${locale}/${fileName}`;
+// }
