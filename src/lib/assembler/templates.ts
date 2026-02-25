69744cfb1af32016e76555ba2be3121875fa6e52
Abhinav5383 <itsabhinav5383@gmail.com>
2026-02-25 09:53:37 +0530

    add fallback template

--- src/lib/assembler/templates.ts
+++ src/lib/assembler/templates.ts
@@ -1,22 +1,24 @@
 // assembling templates for locales other than english
 const ASSEMBLING_TEMPLATE_DEFAULTS = {
-    translation: `import type { Locale } from "~/locales/types";
-
+    translation: `import type { Locale } from "~/locales/types";\n
 export default {} satisfies Locale;`,
 
-    tags: `import type tags from "~/locales/en/tags";
-
+    tags: `import type tags from "~/locales/en/tags";\n
 export default {} satisfies typeof tags;`,
 
-    about: `import type { AboutUsProps } from "~/locales/en/about";
+    about: `import type { AboutUsProps } from "~/locales/en/about";\n
+export const AboutUs = (props: AboutUsProps) => \`\`;`,
 
-export const AboutUs = (props: AboutUsProps) => { };`,
+    terms: `import type { TermsProps } from "~/locales/en/terms";\n
+export const TermsOfUse = (props: TermsProps) => \`\`;`,
 };
 
 export function getAssemblingTemplate(fileName: string, translatingLocaleCode: string | undefined) {
     if (translatingLocaleCode) return translatingLocaleCode;
 
-    return ASSEMBLING_TEMPLATE_DEFAULTS[
-        fileName.split(".")[0] as keyof typeof ASSEMBLING_TEMPLATE_DEFAULTS
-    ];
+    const defaultTemplate =
+        ASSEMBLING_TEMPLATE_DEFAULTS[
+            fileName.split(".")[0] as keyof typeof ASSEMBLING_TEMPLATE_DEFAULTS
+        ];
+    return defaultTemplate ?? "export default {};";
 }
