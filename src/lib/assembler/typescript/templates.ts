d7cee997899b82b72b2dfce48fa7da99108ebc84
Abhinav5383 <itsabhinav5383@gmail.com>
2026-02-27 22:21:13 +0530

    update import locations in the templates

--- src/lib/assembler/typescript/templates.ts
+++ src/lib/assembler/typescript/templates.ts
@@ -3,13 +3,13 @@ const ASSEMBLING_TEMPLATE_DEFAULTS = {
     translation: `import type { Locale } from "~/locales/types";\n
 export default {} satisfies Locale;`,
 
-    tags: `import type tags from "~/locales/en/tags";\n
+    tags: `import type tags from "~/locales/en-US/tags";\n
 export default {} satisfies typeof tags;`,
 
-    about: `import type { AboutUsProps } from "~/locales/en/about";\n
+    about: `import type { AboutUsProps } from "~/locales/en-US/about";\n
 export const AboutUs = (props: AboutUsProps) => \`\`;`,
 
-    terms: `import type { TermsProps } from "~/locales/en/terms";\n
+    terms: `import type { TermsProps } from "~/locales/en-US/terms";\n
 export const TermsOfUse = (props: TermsProps) => \`\`;`,
 };
 
