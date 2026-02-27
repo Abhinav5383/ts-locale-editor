7189bb9af00820bc70a184af032b1c48a2fa2211
Abhinav5383 <itsabhinav5383@gmail.com>
2026-02-27 22:29:36 +0530

    add some missing templates

--- src/lib/assembler/typescript/templates.ts
+++ src/lib/assembler/typescript/templates.ts
@@ -1,4 +1,4 @@
-// assembling templates for locales other than english
+// assembling templates for locales other than en-US (default locale)
 const ASSEMBLING_TEMPLATE_DEFAULTS = {
     translation: `import type { Locale } from "~/locales/types";\n
 export default {} satisfies Locale;`,
@@ -11,6 +11,18 @@ export const AboutUs = (props: AboutUsProps) => \`\`;`,
 
     terms: `import type { TermsProps } from "~/locales/en-US/terms";\n
 export const TermsOfUse = (props: TermsProps) => \`\`;`,
+
+    security: `import type { SecurityProps } from "~/locales/en-US/legal/security";\n
+export const SecurityNotice = (props: SecurityProps) => \`\`;`,
+
+    rules: `import type { RulesProps } from "~/locales/en-US/legal/rules";\n
+export const Rules = (props: RulesProps) => \`\`;`,
+
+    privacy: `import type { PrivacyProps } from "~/locales/en-US/legal/privacy";\n
+export const PrivacyPolicy = (props: PrivacyProps) => \`\`;`,
+
+    copyright: `import type { CopyrightProps } from "~/locales/en-US/legal/copyright";\n
+export const CopyrightPolicy = (props: CopyrightProps) => \`\`;`,
 };
 
 export function getAssemblingTemplate(fileName: string, translatingLocaleCode: string | undefined) {
