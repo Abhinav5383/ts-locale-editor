30274fb473c39051245535f0fda49e603236468e
Abhinav5383 <itsabhinav5383@gmail.com>
2026-03-04 16:16:01 +0530

    use a hashmap for fast lookup

--- src/lib/adapters/utils.ts
+++ src/lib/adapters/utils.ts
@@ -1,4 +1,4 @@
-import { NodeType, type ObjectNode } from "~/lib/types";
+import { NodeType, type ObjectNode, type TranslationNode, type WithKey } from "~/lib/types";
 
 export function sortNodes(node: ObjectNode, refNode: ObjectNode): ObjectNode {
     const result: ObjectNode = {
@@ -6,19 +6,30 @@ export function sortNodes(node: ObjectNode, refNode: ObjectNode): ObjectNode {
         value: [],
     };
 
-    const orderedKeys = refNode.value.map((prop) => prop.key);
+    const orderedKeys: string[] = [];
+
+    const refNodeMap = new Map<string, WithKey<TranslationNode>>();
+    for (const item of refNode.value) {
+        if (!orderedKeys.includes(item.key)) {
+            orderedKeys.push(item.key);
+        }
+        refNodeMap.set(item.key, item);
+    }
+
+    const nodeMap = new Map<string, WithKey<TranslationNode>>();
     for (const item of node.value) {
         if (!orderedKeys.includes(item.key)) {
             orderedKeys.push(item.key);
         }
+        nodeMap.set(item.key, item);
     }
 
     for (const key of orderedKeys) {
-        const val = node.value.find((prop) => prop.key === key);
-        const refVal = refNode.value.find((prop) => prop.key === key);
+        const val = nodeMap.get(key);
+        const refVal = refNodeMap.get(key);
         if (!val) continue;
 
-        if (val.type === NodeType.Object && refVal && refVal.type === NodeType.Object) {
+        if (val.type === NodeType.Object && refVal?.type === NodeType.Object) {
             const sortedChildVals = sortNodes(val, refVal);
             result.value.push({
                 ...val,
